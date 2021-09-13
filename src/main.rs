ff6c247b1cfcad886f11a1de61b643aa12b296f2
David Sherret <dsherret@gmail.com>
2021-09-13 14:35:04 -0400

    Working.

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,50 @@
+use std::io::BufRead;
+use std::io::BufReader;
 use std::ptr;
+use std::io::Write;
 
-use winapi::shared::minwindef::BYTE;
 use winapi::shared::minwindef::FALSE;
 use winapi::shared::minwindef::TRUE;
-use winapi::shared::ntdef::WCHAR;
 use winapi::shared::winerror::S_OK;
 use winapi::um::consoleapi::ClosePseudoConsole;
 use winapi::um::consoleapi::CreatePseudoConsole;
+use winapi::um::fileapi::ReadFile;
+use winapi::um::fileapi::WriteFile;
 use winapi::um::handleapi::CloseHandle;
 use winapi::um::namedpipeapi::CreatePipe;
 use winapi::um::processthreadsapi::CreateProcessW;
+use winapi::um::processthreadsapi::DeleteProcThreadAttributeList;
 use winapi::um::processthreadsapi::InitializeProcThreadAttributeList;
 use winapi::um::processthreadsapi::UpdateProcThreadAttribute;
+use winapi::um::processthreadsapi::LPPROC_THREAD_ATTRIBUTE_LIST;
 use winapi::um::processthreadsapi::PROCESS_INFORMATION;
 use winapi::um::winbase::CREATE_UNICODE_ENVIRONMENT;
 use winapi::um::winbase::EXTENDED_STARTUPINFO_PRESENT;
 use winapi::um::winbase::STARTUPINFOEXW;
-use winapi::um::wincon::WriteConsoleInputW;
 use winapi::um::wincontypes::COORD;
 use winapi::um::wincontypes::HPCON;
-use winapi::um::wincontypes::INPUT_RECORD;
-use winapi::um::wincontypes::KEY_EVENT;
 use winapi::um::winnt::HANDLE;
 
 fn main() {
   let stdin = PseudoPipe::new();
   let stdout = PseudoPipe::new();
-  let console = PseudoConsole::new(stdin, stdout);
+  let mut console = PseudoConsole::new(stdin, stdout);
 
-  console.type_char('y');
-  console.type_char('y');
+  let console_reader = BufReader::new(console.get_reader());
+  let mut lines = console_reader.lines();
+  println!("{}", lines.next().unwrap().unwrap());
+  println!("{}", lines.next().unwrap().unwrap());
+  println!("{}", lines.next().unwrap().unwrap());
+
+  let text = "echo Hello\r\n\x1b[A\r\n";
+  console.write(text.as_bytes()).unwrap();
+
+  println!("{}", lines.next().unwrap().unwrap());
+  println!("{}", lines.next().unwrap().unwrap());
+  println!("{}", lines.next().unwrap().unwrap());
+  println!("{}", lines.next().unwrap().unwrap());
+  println!("{}", lines.next().unwrap().unwrap());
+  println!("{}", lines.next().unwrap().unwrap());
 
   println!("finish")
 }
@@ -49,7 +64,7 @@ impl PseudoPipe {
         CreatePipe(
           &mut read_handle,
           &mut write_handle,
-          std::ptr::null_mut(),
+          ptr::null_mut(),
           0
         ),
         TRUE
@@ -98,43 +113,11 @@ impl PseudoConsole {
       );
       assert_eq!(result, S_OK);
 
+      let mut attribute_list = ProcThreadAttributeList::new(console_handle);
       let mut startup_info: STARTUPINFOEXW = std::mem::zeroed();
       startup_info.StartupInfo.cb =
         std::mem::size_of::<STARTUPINFOEXW>() as u32;
-
-      // discover size required for the list
-      let mut size = 0;
-      assert_eq!(
-        InitializeProcThreadAttributeList(ptr::null_mut(), 1, 0, &mut size),
-        FALSE
-      );
-
-      let mut attribute_list = vec![0u8; size];
-      startup_info.lpAttributeList = attribute_list.as_mut_ptr() as _;
-
-      assert_eq!(
-        InitializeProcThreadAttributeList(
-          startup_info.lpAttributeList,
-          1,
-          0,
-          &mut size,
-        ),
-        TRUE
-      );
-
-      let PROC_THREAD_ATTRIBUTE_PSEUDOCONSOLE = 0x00020016;
-      assert_eq!(
-        UpdateProcThreadAttribute(
-          startup_info.lpAttributeList,
-          0,
-          PROC_THREAD_ATTRIBUTE_PSEUDOCONSOLE,
-          console_handle,
-          std::mem::size_of::<HPCON>(),
-          ptr::null_mut(),
-          ptr::null_mut(),
-        ),
-        TRUE
-      );
+      startup_info.lpAttributeList = attribute_list.as_mut_ptr();
 
       let mut proc_info: PROCESS_INFORMATION = std::mem::zeroed();
       let mut cmd = to_windows_str("C:\\Windows\\System32\\cmd.exe");
@@ -168,29 +151,33 @@ impl PseudoConsole {
     }
   }
 
-  pub fn type_char(&self, char: char) {
-    unsafe {
-      let mut input_record: INPUT_RECORD = std::mem::zeroed();
-      input_record.EventType = KEY_EVENT;
-      input_record.Event.KeyEvent_mut().wRepeatCount = 1;
-      *input_record.Event.KeyEvent_mut().uChar.UnicodeChar_mut() =
-        char as WCHAR;
-
-      let mut events_written = 0;
-      let result = WriteConsoleInputW(
-        self.stdin.write_handle,
-        &mut input_record,
-        1,
-        &mut events_written,
-      );
-      println!(
-        "Failing here :( -- Error: {}",
-        std::io::Error::last_os_error().to_string()
-      );
-      assert_eq!(result, TRUE);
-      assert_eq!(events_written, 1);
-    }
+  pub fn get_reader(&self) -> ConsoleReader {
+    ConsoleReader { read_handle: self.stdout.read_handle }
   }
+}
+
+impl std::io::Write for PseudoConsole {
+    fn write(&mut self, buffer: &[u8]) -> std::io::Result<usize> {
+      unsafe {
+        let mut bytes_written = 0;
+        assert_eq!(
+          WriteFile(
+            self.stdin.write_handle,
+            buffer.as_ptr() as *const _,
+            buffer.len() as u32,
+            &mut bytes_written,
+            ptr::null_mut(),
+          ),
+          TRUE
+        );
+
+        Ok(bytes_written as usize)
+      }
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+      Ok(())
+    }
 }
 
 impl Drop for PseudoConsole {
@@ -200,6 +187,91 @@ impl Drop for PseudoConsole {
       CloseHandle(self.thread_handle);
       CloseHandle(self.process_handle);
     }
+  }
+}
+
+struct ConsoleReader {
+  read_handle: HANDLE,
+}
+
+impl std::io::Read for ConsoleReader {
+  fn read(&mut self, buffer: &mut [u8]) -> std::io::Result<usize> {
+    unsafe {
+      let mut bytes_read = 0;
+      assert_eq!(
+        ReadFile(
+          self.read_handle,
+          buffer.as_mut_ptr() as _,
+          buffer.len() as u32,
+          &mut bytes_read,
+          ptr::null_mut(),
+        ),
+        TRUE
+      );
+      Ok(bytes_read as usize)
+    }
+  }
+}
+
+struct ProcThreadAttributeList {
+  buffer: Vec<u8>,
+}
+
+impl ProcThreadAttributeList {
+  pub fn new(console_handle: HPCON) -> Self {
+    unsafe {
+      // discover size required for the list
+      let mut size = 0;
+      let attribute_count = 1;
+      assert_eq!(
+        InitializeProcThreadAttributeList(
+          ptr::null_mut(),
+          attribute_count,
+          0,
+          &mut size
+        ),
+        FALSE
+      );
+
+      let mut buffer = vec![0u8; size];
+      let attribute_list_ptr = buffer.as_mut_ptr() as _;
+
+      assert_eq!(
+        InitializeProcThreadAttributeList(
+          attribute_list_ptr,
+          attribute_count,
+          0,
+          &mut size,
+        ),
+        TRUE
+      );
+
+      const PROC_THREAD_ATTRIBUTE_PSEUDOCONSOLE: usize = 0x00020016;
+      assert_eq!(
+        UpdateProcThreadAttribute(
+          attribute_list_ptr,
+          0,
+          PROC_THREAD_ATTRIBUTE_PSEUDOCONSOLE,
+          console_handle,
+          std::mem::size_of::<HPCON>(),
+          ptr::null_mut(),
+          ptr::null_mut(),
+        ),
+        TRUE
+      );
+
+      ProcThreadAttributeList { buffer }
+    }
+  }
+
+  pub fn as_mut_ptr(&mut self) -> LPPROC_THREAD_ATTRIBUTE_LIST {
+    self.buffer.as_mut_slice().as_mut_ptr() as *mut _
+  }
+}
+
+impl Drop for ProcThreadAttributeList {
+  fn drop(&mut self) {
+    unsafe { DeleteProcThreadAttributeList(self.as_mut_ptr()) };
   }
 }
 
